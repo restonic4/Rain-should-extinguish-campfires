ec35d73cfe7feea1d01bd3c6caa12e53bba50051
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 14:47:13 +0100

    Update

--- common/src/main/java/me/restonic4/rsec/RainShouldExtinguishCampfires.java
+++ common/src/main/java/me/restonic4/rsec/RainShouldExtinguishCampfires.java
@@ -1,14 +1,16 @@
 package me.restonic4.rsec;
 
-import dev.architectury.platform.Platform;
-import me.restonic4.restapi.RestApi;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RainShouldExtinguishCampfires
 {
 	public static final String MOD_ID = "rsec";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static void init() {
-		RestApi.Log("Did you know that rain should extinguish campfires?");
-		RestApi.Log("Yeah, this mod fixes that. With one simple mixin, that's cool.");
+		LOGGER.info("----- Rain should extinguish campfires --- 1.19.2 --- Cant use RestApi -----");
+		LOGGER.info("Did you know that rain should extinguish campfires?");
+		LOGGER.info("Yeah, this mod fixes that. With one simple mixin, that's cool.");
 	}
 }
