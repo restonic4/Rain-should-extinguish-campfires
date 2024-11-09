4d6c9572abcdf22bd9ea48e1890a1dd0265dd66f
restonic4 <marcosruizlerma@gmail.com>
2024-11-09 14:19:46 +0100

    Update

---
+++ src/main/java/com/restonic4/rsec/RainCampfiresFabric.java
@@ -0,0 +1,17 @@
+package com.restonic4.rsec;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RainCampfiresFabric implements ModInitializer {
+    public static final String MOD_ID = "rsec";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("----- Rain should extinguish campfires --- 1.19.2 --- Cant use RestApi -----");
+        LOGGER.info("Did you know that rain should extinguish campfires?");
+        LOGGER.info("Yeah, this mod fixes that. With one simple mixin, that's cool.");
+    }
+}
