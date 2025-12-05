f48c585ee39f45d5d803d7eb0330e709eb4478d7
restonic4 <marcosruizlerma@gmail.com>
2025-12-05 11:14:11 +0100

    Namespaces fix

---
+++ common/src/main/java/com/example/my_mod/Constants.java
@@ -0,0 +1,11 @@
+package com.example.my_mod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "my_mod";
+	public static final String MOD_NAME = "My mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
